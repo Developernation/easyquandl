9d984c8498ff402cc7ab11c350ab594569c94c4f
Developernation <simeon.f.anderson@gmail.com>
2019-01-10 20:49:39 -0500

    First commit

---
+++ qndlvars.py
@@ -0,0 +1,17 @@
+#QANDL REALESTATE DATA URLS
+STATECODES_URL = 'https://s3.amazonaws.com/quandl-production-static/zillow/areas_state.txt'
+COUNTYCODES_URL = 'https://s3.amazonaws.com/quandl-production-static/zillow/areas_county.txt'
+METROCODES_URL = 'https://s3.amazonaws.com/quandl-production-static/zillow/areas_metro.txt'
+CITYCODES_URL = 'https://s3.amazonaws.com/quandl-production-static/zillow/areas_city.txt'
+NBHCODES_URL = 'https://s3.amazonaws.com/quandl-production-static/zillow/areas_neighborhood.txt'
+INDCODES_URL = 'https://s3.amazonaws.com/quandl-production-static/zillow/indicators.csv'
+#QUERY CODES
+AREA_TYPE = {
+         'STATE':'S',
+         'County':'CO',
+         'Greater Metropolitan Area':'M',
+         'City':'C',
+         'Neighborhood':'N',
+         'Zip Code':'Z'
+        }
+#-------------------------------------------------------------------------------------------------
