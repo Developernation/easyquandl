e5cad20a263680ff72350e9c9704d68c360fcd1c
rbzargon <cking10@worcester.edu>
2019-02-03 05:50:36 -0500

    Delete files
    Moved functionality to model/location_codes to simplify

--- src/utils/location/i_location_parser.py
+++
@@ -1,9 +0,0 @@
-from interface import Interface
-from typing import Dict, List, Union
-
-
-class ILocationParser(Interface):
-
-    @staticmethod
-    def parse(page: str, area_type: str) -> List[Dict[str, Union[str, int]]]:
-        pass
